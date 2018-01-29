86fe40e6bf84d9c13309dafc4f94bed03114bf17
Oliver Sherouse <oliver@oliversherouse.com>
2018-01-29 09:17:30 -0500

    peterson post and youtube code

--- js/main.js
+++ js/main.js
@@ -14,10 +14,28 @@ $(function(){
         });
     }
 
+    site.resize_videos = function() {
+        site.allVideos.each(function() {
+            var el = $(this);
+            var newWidth = el.parent().width();
+            el
+              .width(newWidth)
+              .height(newWidth * el.data('aspectRatio'));
+        });
+    }
+
     site.toggle_menu = function(){
         $('#fullnav').toggle(400);
     }
 
+    site.allVideos = $("iframe[src^='https://www.youtube.com']");
+    site.allVideos.each(function() {
+      $(this)
+        .data('aspectRatio', this.height / this.width)
+        .removeAttr('height')
+        .removeAttr('width');
+    });
+    site.resize_videos();
     site.place_footnotes();
     $('#navexpand').click(site.toggle_menu);
 });
@@ -27,3 +45,4 @@ anchors.options = {
     // icon: '#',
 };
 anchors.add('.post h2,h3,h4,h5,h6');
+
