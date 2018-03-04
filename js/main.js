f27fa0c9dd01a00b8211f064ab3c2014689dbcd7
Oliver Sherouse <oliver@oliversherouse.com>
2018-03-03 19:37:27 -0500

    js reorg, pagination, logo

--- js/main.js
+++ js/main.js
@@ -1,47 +1,69 @@
-var site = {}
-$(function() {
-    site.place_footnotes = function() {
-        $('article').each(function() {
-            var refs = $(this).find('.footnote');
-            var notes = $(this).find('.footnotes ol li');
-            var gutter = $(this).offset().left + $(this).width();
-            for (var i = 0; i < notes.length; i++) {
-                $(notes[i]).css({
-                    top: $(refs[i]).parent().position()
-                        .top,
-                    left: gutter,
-                });
-            }
+/*
+ * Footnotes
+ */
+let footnotes = {};
+footnotes.place = function() {
+    const first_header = $('article>header').first()
+    const gutter = first_header.position().left + first_header.width()
+
+    $('.footnotes>ol>li').each(function() {
+        const note = $(this);
+        const superscript = note.closest('article').find(
+            note
+            .find('a.reversefootnote')
+            .attr('href')
+            .replace('fnref:', 'fnref\\:')
+        );
+        note.css({
+            top: superscript.position().top,
+            left: gutter,
         });
-    }
+    });
+};
 
-    site.resize_videos = function() {
-        site.allVideos.each(function() {
-            var el = $(this);
-            var newWidth = el.parent().width();
-            el
-                .width(newWidth)
-                .height(newWidth * el.data('aspectRatio'));
-        });
-    }
+footnotes.init = function() {
+    $(window).resize(footnotes.place);
+    footnotes.place();
+};
 
-    site.toggle_menu = function() {
-        $('#fullnav').toggle(400);
-    }
+/* 
+ * Responsive YouTube Videos 
+ */
+let yt_resizer = {};
+yt_resizer.get_videos = function() {
+    return $("iframe[src^='https://www.youtube.com']");
+};
 
-    site.allVideos = $("iframe[src^='https://www.youtube.com']");
-    site.allVideos.each(function() {
+yt_resizer.resize = function() {
+    yt_resizer.get_videos().each(function() {
+        const el = $(this);
+        const newWidth = el.prev().width();
+        el.width(newWidth).height(newWidth * el.data('aspectRatio'));
+    });
+};
+
+yt_resizer.init = function() {
+    yt_resizer.get_videos().each(function() {
         $(this)
             .data('aspectRatio', this.height / this.width)
             .removeAttr('height')
             .removeAttr('width');
     });
-    site.resize_videos();
-    site.place_footnotes();
-    $('#navexpand').click(site.toggle_menu);
-});
-
-anchors.options = {
-    placement: 'left',
+    $(window).resize(yt_resizer.resize);
+    yt_resizer.resize();
 };
-anchors.add('.prose h2,h3,h4,h5,h6');
+
+/*
+ * Main Init
+ */
+$(function() {
+    yt_resizer.init();
+    footnotes.init();
+    $('.navbar-burger').click(function() {
+        $(".navbar-menu").toggleClass("is-active")
+    });
+    anchors.options = {
+        placement: 'left',
+    };
+    anchors.add('.prose h2,h3,h4,h5,h6');
+});
