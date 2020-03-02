363b3e4aa01cebc770f24cebabcc597c43adb88f
Oliver Sherouse <oliver@oliversherouse.com>
2020-03-01 21:56:07 -0500

    various style updates, remove jQuery, bulma 0.8

--- js/main.js
+++ js/main.js
@@ -1,69 +1,78 @@
 /*
  * Footnotes
  */
-let footnotes = {};
-footnotes.place = function() {
-    const first_header = $('article>div.content>p').first()
-    const gutter = first_header.width()
+const footnotes = {}
+footnotes.place = function () {
+  const firstHeader = document.querySelector('article>div.content>p')
+  const gutter = firstHeader.offsetWidth
+  const footnotes = document.querySelectorAll('.footnotes>ol>li')
+  for (const note of footnotes) {
+    const article = note.closest('article')
+    const superscript = article.querySelector(
+      note
+        .querySelector('.reversefootnote')
+        .getAttribute('href')
+        .replace('fnref:', 'fnref\\:')
+    )
+    note.style.top = `${superscript.offsetTop}px`
+    note.style.left = `${gutter}px`
+  }
+}
 
-    $('.footnotes>ol>li').each(function() {
-        const note = $(this);
-        const superscript = note.closest('article').find(
-            note
-            .find('a.reversefootnote')
-            .attr('href')
-            .replace('fnref:', 'fnref\\:')
-        );
-        note.css({
-            top: superscript.position().top,
-            left: gutter,
-        });
-    });
-};
+footnotes.init = function () {
+  const imgs = document.querySelectorAll('img')
+  const iframes = document.querySelectorAll('iframe')
+  for (const img of imgs) { img.addEventListener('load', footnotes.place) }
+  for (const iframe of iframes) { iframe.addEventListener('load', footnotes.place) }
+  window.addEventListener('resize', footnotes.place)
+  footnotes.place()
+}
 
-footnotes.init = function() {
-    $(window).resize(footnotes.place);
-    footnotes.place();
-};
+async function addBulmaImages () {
+  const articles = document.getElementsByTagName('article')
+  for (const article of articles) {
+    const imgs = article.getElementsByTagName('img')
+    for (const img of imgs) {
+      const figure = document.createElement('figure')
+      figure.appendChild(img.cloneNode(true))
+      const parent = img.parentElement
+      figure.classList.add('image')
+      if (parent.tagName === 'P') {
+        parent.replaceWith(figure)
+      } else {
+        img.replaceWith(figure)
+      }
+    }
 
-/* 
- * Responsive YouTube Videos 
- */
-let yt_resizer = {};
-yt_resizer.get_videos = function() {
-    return $("iframe[src^='https://www.youtube.com']");
-};
-
-yt_resizer.resize = function() {
-    yt_resizer.get_videos().each(function() {
-        const el = $(this);
-        const newWidth = el.prev().width();
-        el.width(newWidth).height(newWidth * el.data('aspectRatio'));
-    });
-};
-
-yt_resizer.init = function() {
-    yt_resizer.get_videos().each(function() {
-        $(this)
-            .data('aspectRatio', this.height / this.width)
-            .removeAttr('height')
-            .removeAttr('width');
-    });
-    $(window).resize(yt_resizer.resize);
-    yt_resizer.resize();
-};
+    const iframes = article.getElementsByTagName('iframe')
+    for (const iframe of iframes) {
+      const figure = document.createElement('figure')
+      const iframeClone = iframe.cloneNode(true)
+      iframeClone.classList.add('has-ratio')
+      figure.appendChild(iframeClone)
+      const parent = iframe.parentElement
+      figure.classList.add('image')
+      figure.classList.add('is-16by9')
+      if (parent.tagName === 'P') {
+        parent.replaceWith(figure)
+      } else {
+        iframe.replaceWith(figure)
+      }
+    }
+  }
+}
 
 /*
  * Main Init
  */
-$(function() {
-    yt_resizer.init();
-    footnotes.init();
-    $('.navbar-burger').click(function() {
-        $(".navbar-menu").toggleClass("is-active")
-    });
-    anchors.options = {
-        placement: 'left',
-    };
-    anchors.add('.prose h2,h3,h4,h5,h6');
-});
+async function mainInit () {
+  footnotes.init()
+  document.querySelector('.navbar-burger').addEventListener('click', () => document.querySelector('.navbar-menu').classList.toggle('is-active'))
+  addBulmaImages()
+  // anchors.options = {
+  //   placement: 'left'
+  // }
+  // anchors.add('.prose h2,h3,h4,h5,h6')
+}
+
+document.addEventListener('DOMContentLoaded', mainInit)
